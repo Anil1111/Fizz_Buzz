16f37e3ee81c1bb99166d8aaac8d01a68fd7ab5a
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-08-20 10:17:30 +0530

    Effective Logic

--- Fizz_Buzz/Program.cs
+++ Fizz_Buzz/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace Fizz_Buzz
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Effective Program Logic");
+        for (int i = 1; i < 255; i++)
         {
-            Console.WriteLine("Hello World!");
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0 && i % 5 != 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 3 != 0 && i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+            i++;
         }
     }
+
 }
+
